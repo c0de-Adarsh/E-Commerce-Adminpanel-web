415d429d0a57a47c0aad4851e0d7d2ef636f92b5
Adarsh <152369806+c0de-Adarsh@users.noreply.github.com>
2025-02-02 21:33:55 +0530

    complete orders controllers

---
+++ Backend/Controllers/paymentController.js
@@ -0,0 +1,1 @@
+const strip = require('stripe')
--- Backend/Routes/Route.js
+++ Backend/Routes/Route.js
@@ -6,6 +6,7 @@ const router = express.Router()
 const {registerUser,loginUser, forgotPassword, resetPassword, isLogin, getUserDetails, updatePassword, updateProfile, getAllUsers, updateUser, getUser, deleteUser} = require('../Controllers/userController')
 const { jwtAuthMiddleWare, authorizationRole } = require('../jwt')
 const { createProduct, getAllProducts, getCategoryProducts, getAdminProducts, deleteProduct, updateProduct, getProductDetails, createProductReview, getProductReview, getEveryProduct } = require('../Controllers/productController')
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require('../Controllers/orderControllers')
 
 router.route('/registeruser').post(registerUser)
 router.route('/loginuser').post(loginUser)
@@ -37,6 +38,19 @@ router.route('/reeview/create').post(jwtAuthMiddleWare,createProductReview)
 router.route('/reviews').get(getProductReview)
 router.route('/reviews').delete(jwtAuthMiddleWare,getProductReview)
 
+
+//payment routes
 router.route('/geteveryproduct').get(getEveryProduct)
+
+
+//product route
+
+router.route('/order/new').post(jwtAuthMiddleWare,newOrder)
+router.route('/order/:id').get(jwtAuthMiddleWare,getSingleOrder)
+router.route('/orders/me').get(jwtAuthMiddleWare,myOrders)
+router.route('/admin/orders').get(jwtAuthMiddleWare,authorizationRole('admin'),getAllOrders)
+router.route('/admin/order/:id').put(jwtAuthMiddleWare,authorizationRole('admin'),updateOrder)
+router.route('/admin/order/:id').delete(jwtAuthMiddleWare,deleteOrder)
+
 
 module.exports = router
