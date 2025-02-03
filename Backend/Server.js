454881bfe2277c77171006c68fc2521f8b92388f
Adarsh <152369806+c0de-Adarsh@users.noreply.github.com>
2025-02-03 13:10:59 +0530

    frotend setup with redux

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const db = require('./db')
+const cors = require('cors')
 const route = require('./Routes/Route')
 const fileUpload = require('express-fileupload')
 const cloudinary = require('cloudinary').v2
 const PORT = process.env.PORT || 5000
-
 
 
 cloudinary.config({
@@ -16,7 +16,10 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-
+app.use(cors({
+    origin:'*',
+    credentials:true
+}))
 app.use(express.json({ limit: '10mb' }));
 
 app.use(fileUpload({
@@ -24,6 +27,23 @@ app.use(fileUpload({
     tempFileDir: '/tmp/'
 }));
 app.use(route)
+
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`)
+    console.log("Shutting down the server due to uncaught exception")
+
+    process.exit(1)
+})
+
+
+//server unexcpected server crash ko rukne ke liye 
+process.on("unhandledRejection", err => {
+    console.log(`Error: ${err.message}`)
+    console.log("Shutting down the server due to unhandled promise rejection")
+    server.close(()=>{
+        process.exit(1)
+    })
+})
 app.get('/',(req , res)=>{
     res.send('Hello world')
 })
---
+++ Frontend/src/Slice/UserSlice.js
@@ -0,0 +1,162 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+  const userSlice = createSlice({
+      name:'user',
+      initialState:{
+        loading:false,
+        isAuthenticated: false,
+        user: null,
+        error: null,
+        isLogin: false,
+        me: null,
+        isUpdated: false ,
+        allUsers: [],
+        userDetails: {
+            _id: "",
+            name: "",
+            email: "",
+            password: "",
+            avtar: [], 
+            role: "",
+            createdAt: "",
+            __v: 0,
+          } ,
+        success: false,
+      },
+       
+      reducers:{
+        loginRequest:(state) =>{
+            state.loading = true;
+            state.isAuthenticated = false;
+        },
+        loginSuccess:(state,action) =>{
+            state.loading = false;
+            state.isAuthenticated = true;
+            state.user = action.payload;
+            state.isLogin = true;
+        },
+        loginFail:(state,action) =>{
+            state.loading = false;
+            state.isAuthenticated = false;
+            state.user = null;
+            state.error = action.payload
+            state.wrongCredentialsErr = true
+        },
+        registerRequest:(state) =>{
+            state.loading = true;
+            state.isAuthenticated = false
+        },
+        registerSuccess:(state,action)=>{
+            state.loading = false;
+            state.isAuthenticated = true;
+            state.user = action.payload;
+            state.isLogin = true
+        },
+        registerFail:(state,action) =>{
+            state.loading = false;
+            state.isAuthenticated = false;
+            state.user = null;
+            state.error = action.payload
+            state.userExistErr = true;
+        },
+        setIsLoginTrue:(state)=>{
+            state.isLogin = true;
+        },
+        setIsLoginFalse:(state)=>{
+            state.isLogin = false;
+        },
+        getMeRequest:(state) =>{
+            state.loading = true;
+            
+        },
+        getMeSuccess:(state,action)=>{
+            state.loading = false;
+            state.me = action.payload;
+        },
+        getMeFail:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload
+            state.me = null
+        },
+        updateProfileRequest:(state) =>{
+            state.loading = true
+        },
+        updateProfileSuccess:(state)=>{
+            state.loading = false;
+            state.isUpdated = true
+        },
+        updateProfileFail:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload
+        },
+        updateProfileRest:(state)=>{
+            state.isUpdated = false
+        },
+
+        changePasswordRequest:(state)=>{
+            state.loading = true;
+        },
+        changePasswordSuccess:(state)=>{
+            state.loading = false;
+        },
+        changePasswordFail:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        },
+        allUserRequest:(state)=>{
+            state.loading = true;
+        },
+        allUserSuccess:(state,action)=>{
+            state.loading = false;
+            state.allUsers = action.payload
+        },
+        allUserFail:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload
+        },
+
+        userDetailsRequest:(state)=>{
+            state.loading = true;
+        },
+        userDetailsSuccess:(state,action)=>{
+            state.loading = false;
+            state.userDetails = action.payload;
+        },
+        userDetailsFail:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        },
+        userUpdatedRequest:(state)=>{
+            state.loading = true;
+        },
+        userUpdatedSuccess:(state,action)=>{
+            state.loading = false;
+            state.success = action.payload.success; 
+        },
+        userUpdatedFail:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        },
+        userDeleteRequest:(state)=>{
+            state.loading = true;
+        },
+        userDeleteSuccess:(state,action)=>{
+            state.loading = false;
+            state.success = true;
+        },
+        userDeleteFail:(state,action)=>{
+            state.loading = false;
+            state.error = action.payload
+        },
+
+        clearErrors:(state)=>{
+            state.error = null
+        }
+      }
+  })
+
+  export const {loginRequest , loginSuccess , loginFail , registerRequest , registerSuccess , registerFail,setIsLoginTrue,setIsLoginFalse , getMeRequest,getMeSuccess,getMeFail, updateProfileRequest,updateProfileSuccess,updateProfileFail,updateProfileRest,userDeleteRequest,userDeleteSuccess,userDeleteFail,userDetailsRequest,userDetailsSuccess,userDetailsFail,userUpdatedRequest,userUpdatedSuccess,userUpdatedFail,allUserRequest,allUserSuccess,allUserFail,changePasswordRequest, changePasswordSuccess,changePasswordFail} = userSlice.actions;
+
+  export default userSlice.reducer
